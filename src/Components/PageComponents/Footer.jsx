f0de85dbaedb7033a2a8a8805a73d21127aaec44
Debashis Roy <goodquote39@hotmail.com>
2023-10-16 17:22:36 +0600

    working with footer

---
+++ src/Components/PageComponents/Footer.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-24">
+      <p>Footer</p>
+    </div>
+  );
+};
+
+export default Footer;
