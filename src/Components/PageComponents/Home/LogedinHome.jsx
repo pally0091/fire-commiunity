218cf9aeda05e9da9d91a416ff0a88294c79c235
Debashis Roy <goodquote39@hotmail.com>
2023-10-17 20:56:59 +0600

    wirking on loged home

--- src/Components/PageComponents/Home/LogedinHome.jsx
+++ src/Components/PageComponents/Home/LogedinHome.jsx
@@ -2,9 +2,22 @@
 import React from "react";
 
 const LogedinHome = () => {
+  const user = "A";
   return (
-    <div>
-      <p>Home</p>
+    <div className="w-[95%] mx-auto">
+      <div className="flex justify-between p-8 items-center">
+        <div className="w-1/2 text-left">
+          <h5 className="font-bold">Hi, {user}</h5>
+          <p className="text-[#9CA3AF]">Whats new with you</p>
+        </div>
+        <div className="w-1/2">
+          <input
+            className="w-full rounded-lg p-2"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+      </div>
     </div>
   );
 };
