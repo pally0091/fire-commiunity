cecb17d6bcc2b1b756150ad7f5e78c143f4b6614
Debashis Roy <goodquote39@hotmail.com>
2023-10-18 15:36:13 +0600

    events card set

--- src/Components/InpageComponents/Slider.jsx
+++ src/Components/InpageComponents/Slider.jsx
@@ -1,8 +1,9 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef } from "react";
 
-const Slider = () => {
+const Slider = ({ children, className }) => {
   const slider = useRef(null);
 
   const handleMouseScroll = (e) => {
@@ -11,32 +12,28 @@ const Slider = () => {
       e.preventDefault();
     }
   };
+
   useEffect(() => {
     if (slider.current) {
       slider.current.addEventListener("wheel", handleMouseScroll, {
         passive: false,
       });
     }
+
     return () => {
       if (slider.current) {
         slider.current.removeEventListener("wheel", handleMouseScroll);
       }
     };
   }, []);
+
   return (
-    <div className="flex flex-row gap-10">
+    <div className={`flex flex-row gap-10 ${className}`}>
       <div
         className="w-[90%] mx-auto overflow-x-auto whitespace-nowrap scrollbar"
         ref={slider}
       >
-        <div className="card w-[60%] inline-block bg-white mx-5 rounded-md p-2">
-          <h2>Card 1</h2>
-          <p>This is the content for Card 1.</p>
-        </div>
-        <div className="card w-[60%] inline-block bg-white mx-5 rounded-md p-2">
-          <h2>Card 2</h2>
-          <p>This is the content for Card 2.</p>
-        </div>
+        {children}
       </div>
     </div>
   );
--- src/Components/PageComponents/Home/LogedinHome.jsx
+++ src/Components/PageComponents/Home/LogedinHome.jsx
@@ -1,8 +1,36 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Slider from "../../InpageComponents/Slider";
+import shape from "../../../assets/eventcardshape.png";
 
 const LogedinHome = () => {
+  const events = [
+    {
+      id: 1,
+      cat: "Event",
+      event: "Summer Music Festival",
+      date: "2023-08-15",
+    },
+    {
+      id: 2,
+      cat: "Webiner",
+      event: "Webiner Conference",
+      date: "2023-09-20",
+    },
+    {
+      id: 3,
+      cat: "Event",
+      event: "Art Gallery Opening",
+      date: "2023-10-05",
+    },
+    {
+      id: 4,
+      cat: "Webiner",
+      event: "Webiner Workshop",
+      date: "2023-11-10",
+    },
+  ];
+
   const user = "A";
   return (
     <div className="w-[95%] mx-auto">
@@ -20,7 +48,32 @@ const LogedinHome = () => {
         </div>
       </div>
       <div className="mt-5">
-        <Slider></Slider>
+        <Slider>
+          {events.map((event) => (
+            <div
+              key={event.id}
+              className="card  w-[60%] inline-block bg-white mx-5 rounded-md p-5"
+            >
+              <div className="flex justify-between">
+                <div>
+                  <p className="bg-[#ff6f61] w-1/2 text-white rounded-lg">
+                    {event.cat}
+                  </p>
+                  <h6 className="my-2 font-bold text-left">{event.event}</h6>
+                  <p className="bg-[#3b82f6] w-2/3 text-white rounded-full">
+                    {event.date}
+                  </p>
+                </div>
+                <div>
+                  <img
+                    src={shape}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
